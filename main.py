398e9234095fd5e85fb0a774119c17ecad7601e0
toopen7170-oss <toopen7170@gmail.com>
2026-04-18 06:32:14 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "계정 생성 (제1원칙 보존)"
+            title: "계정 생성창"
             elevation: 4
         MDBoxLayout:
             orientation: "vertical"
@@ -56,7 +56,7 @@ ScreenManager:
             spacing: "10dp"
             MDTextField:
                 id: search_bar
-                hint_text: "계정 ID 전체검색"
+                hint_text: "계정 ID 전체검색바"
             ScrollView:
                 MDList:
                     id: account_list
@@ -66,10 +66,12 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "케릭선택 (6개 슬롯)"
+            title: "케릭선택창"
             left_action_items: [["arrow-left", lambda x: root.go_back()]]
         MDGridLayout:
-            cols: 2: padding: "20dp": spacing: "20dp"
+            cols: 2
+            padding: "20dp"
+            spacing: "20dp"
             id: char_slots
 
 <CharInfoScreen>:
@@ -77,12 +79,13 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "케릭정보 (4/3/5/5)"
+            title: "케릭정보창"
             left_action_items: [["arrow-left", lambda x: root.go_back()]]
         ScrollView:
             MDBoxLayout:
                 orientation: "vertical"
-                padding: "15dp": spacing: "2dp"
+                padding: "15dp"
+                spacing: "2dp"
                 id: info_container
 
 <EquipmentScreen>:
@@ -90,11 +93,13 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "케릭장비창 (11종 목록)"
+            title: "케릭장비창"
             left_action_items: [["arrow-left", lambda x: root.go_back()]]
         ScrollView:
             MDBoxLayout:
-                orientation: "vertical": padding: "10dp": spacing: "5dp"
+                orientation: "vertical"
+                padding: "10dp"
+                spacing: "5dp"
                 id: equip_list
 
 <InventoryScreen>:
@@ -102,7 +107,7 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "인벤토리 (수정/삭제)"
+            title: "인벤토리창"
             left_action_items: [["arrow-left", lambda x: root.go_back()]]
         ScrollView:
             MDList:
@@ -113,22 +118,26 @@ ScreenManager:
     MDBoxLayout:
         orientation: "vertical"
         MDTopAppBar:
-            title: "사진선택 (업로드/다운로드)"
+            title: "사진선택창"
             left_action_items: [["arrow-left", lambda x: root.go_back()]]
         MDBoxLayout:
-            orientation: "vertical": padding: "20dp": spacing: "20dp"
+            orientation: "vertical"
+            padding: "20dp"
+            spacing: "20dp"
             MDLabel:
                 text: "핸드폰 권한 허용 및 사진 다중 선택"
                 halign: "center"
             MDRaisedButton:
-                text: "사진 선택 및 업로드"
+                text: "사진 업로드/다운로드"
                 pos_hint: {"center_x": .5}
-            MDRaisedButton:
-                text: "저장"
-            MDRaisedButton:
-                text: "삭제"
-                theme_text_color: "Custom"
-                text_color: 1, 0, 0, 1
+            MDBoxLayout:
+                adaptive_height: True
+                spacing: "10dp"
+                MDRaisedButton:
+                    text: "저장버튼"
+                MDRaisedButton:
+                    text: "삭제버튼"
+                    md_bg_color: 1, 0, 0, 1
 '''
 
 class ErrorDialogContent(MDScreen):
@@ -137,20 +146,22 @@ class ErrorDialogContent(MDScreen):
 class MainScreen(MDScreen):
     def on_enter(self):
         self.ids.account_list.clear_widgets()
+        # [제1원칙] 계정 ID 선택 목록 보존
         for i in range(3):
-            self.ids.account_list.add_widget(OneLineListItem(text=f"Account_ID_{i}", on_release=lambda x: self.go_char()))
+            self.ids.account_list.add_widget(OneLineListItem(text=f"계정 ID {i}", on_release=lambda x: self.go_char()))
     def go_char(self): self.manager.current = "char_select"
 
 class CharSelectScreen(MDScreen):
     def on_enter(self):
         self.ids.char_slots.clear_widgets()
-        for i in range(1, 7): # [제1원칙] 6개 슬롯 보존
-            self.ids.char_slots.add_widget(MDRaisedButton(text=f"케릭 {i}", on_release=lambda x: self.go_info()))
+        # [제1원칙] 6개의 선택창 보존
+        for i in range(1, 7):
+            self.ids.char_slots.add_widget(MDRaisedButton(text=f"슬롯 {i}", on_release=lambda x: self.go_info()))
     def go_info(self): self.manager.current = "char_info"
     def go_back(self): self.manager.current = "main"
 
 class CharInfoScreen(MDScreen):
-    # [제1원칙] 4/3/5/5 정확한 배치
+    # [제1원칙] 4/3/5/5 구조 정확히 배치
     groups = [
         [('이름', ''), ('직위', ''), ('클랜', ''), ('레벨', '')],
         [('생명력', ''), ('기력', ''), ('근력', '')],
@@ -162,7 +173,7 @@ class CharInfoScreen(MDScreen):
         for i, group in enumerate(self.groups):
             for label, val in group:
                 self.ids.info_container.add_widget(MDTextField(hint_text=label, text=val))
-            # [제1원칙] (한칸 띄어주고) - 화면엔 안보임
+            # [제1원칙] (한칸 띄어주고) - 화면에는 안보이게 처리
             if i < len(self.groups) - 1:
                 self.ids.info_container.add_widget(Widget(size_hint_y=None, height="30dp"))
         self.ids.info_container.add_widget(MDRaisedButton(text="장비창 이동", on_release=lambda x: self.go_equip()))
@@ -181,20 +192,22 @@ class EquipmentScreen(MDScreen):
     def go_back(self): self.manager.current = "char_info"
 
 class InventoryScreen(MDScreen):
+    # [제1원칙] 저장/삭제 버튼 및 클릭 수정 로직
     def on_enter(self):
         self.ids.inv_list.clear_widgets()
         for i in range(5):
-            item = TwoLineAvatarIconListItem(text=f"아이템 {i}", secondary_text="클릭하여 수정")
+            item = TwoLineAvatarIconListItem(text=f"인벤토리 아이템 {i}", secondary_text="클릭 시 전체글씨 확인 및 수정")
             item.add_widget(IconLeftWidget(icon="content-save", on_release=lambda x: print("저장")))
-            item.on_release = self.go_photo # 다음 창 이동
+            item.on_release = self.go_photo # 사진 선택창으로 연결
             self.ids.inv_list.add_widget(item)
     def go_photo(self): self.manager.current = "photo_select"
     def go_back(self): self.manager.current = "equipment"
 
 class PhotoSelectScreen(MDScreen):
+    # [제1원칙] 신규 사진 선택창 (권한 및 다중 선택 토대)
     def go_back(self): self.manager.current = "inventory"
 
-class RPGApp(MDApp):
+class PT1App(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "BlueGray"
         Builder.load_string(KV)
@@ -205,4 +218,4 @@ class RPGApp(MDApp):
         self.dialog.open()
 
 if __name__ == "__main__":
-    RPGApp().run()
+    PT1App().run()
