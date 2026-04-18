22396bfb223f4ee9d748d1e7f77a3766085715c9
toopen7170-oss <toopen7170@gmail.com>
2026-04-18 10:01:07 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -1,215 +1,123 @@
+import os
 import sys
 import traceback
-from kivy.lang import Builder
-from kivy.uix.widget import Widget
-from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.screenmanager import MDScreenManager
-from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.list import OneLineListItem, TwoLineAvatarIconListItem, IconLeftWidget
-from kivy.properties import StringProperty
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+from kivy.core.text import LabelBase
+from kivy.properties import StringProperty, ListProperty
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
 
-# [무한 검증] 실시간 오류 포착 및 리포팅 시스템
-def global_exception_handler(exctype, value, tb):
-    err_msg = "".join(traceback.format_exception(exctype, value, tb))
-    try:
-        app = MDApp.get_running_app()
-        if app: app.show_error_popup(err_msg)
-    except: pass
+# [이미지 1-3] 자가 진단: 폰트 등록 및 예외 처리
+try:
+    if os.path.exists("font.ttf"):
+        LabelBase.register(name="CustomFont", fn_regular="font.ttf")
+    else:
+        print("[자가진단] 경고: font.ttf 파일이 없습니다.")
+except Exception as e:
+    print(f"[자가진단] 폰트 등록 오류: {e}")
 
-sys.excepthook = global_exception_handler
+# [이미지 4-7] S26 Ultra 해상도 대응: 수직 정중앙 정렬 보정 입력창
+class CustomTextInput(TextInput):
+    """S26 Ultra 해상도 대응: 글자 수직 정중앙 정렬 보정"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if os.path.exists("font.ttf"):
+            self.font_name = "CustomFont"
+        self.multiline = False
+        self.font_size = '18sp'
+        self.cursor_color = get_color_from_hex('#1a4361')
+        self.background_color = (1, 1, 1, 0.9)
+        self.bind(size=self._update_padding)
 
-KV = '''
-<ErrorDialogContent>:
-    orientation: "vertical"
-    spacing: "12dp"
-    size_hint_y: None
-    height: "300dp"
-    ScrollView:
-        MDLabel:
-            text: root.error_text
-            size_hint_y: None
-            height: self.texture_size[1]
-            theme_text_color: "Error"
-            font_style: "Caption"
+    def _update_padding(self, *args):
+        # 정밀 계산: (칸 높이 / 2) - (글자 높이 / 2)
+        self.padding_y = [self.height / 2.0 - (self.line_height / 2.0), 0]
 
-ScreenManager:
-    MainScreen:
-    CharSelectScreen:
-    CharInfoScreen:
-    EquipmentScreen:
-    InventoryScreen:
-    PhotoSelectScreen:
+# [이미지 8] 모든 화면의 기초가 되는 배경 이미지 설정 클래스
+class BaseScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            # 배경 이미지(bg.png) 설정 로직 고착
+            self.bg_rect = Rectangle(source='bg.png', size=Window.size, pos=self.pos)
+        self.bind(size=self._update_bg)
 
-<MainScreen>:
-    name: "main"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "계정 생성창 (ID 선택/전체검색)"
-            elevation: 4
-        MDBoxLayout:
-            orientation: "vertical"
-            padding: "10dp"
-            spacing: "10dp"
-            MDTextField:
-                id: search_bar
-                hint_text: "계정 ID 전체검색바"
-            ScrollView:
-                MDList:
-                    id: account_list
+    def _update_bg(self, *args):
+        self.bg_rect.size = self.size
+        self.bg_rect.pos = self.pos
 
-<CharSelectScreen>:
-    name: "char_select"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "케릭선택창 (6개 슬롯)"
-            left_action_items: [["arrow-left", lambda x: root.go_back()]]
-        MDGridLayout:
-            cols: 2
-            padding: "20dp"
-            spacing: "20dp"
-            id: char_slots
+# [이미지 8-11] 메인 화면: 계정 검색 및 생성 리스트
+class MainScreen(BaseScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = BoxLayout(orientation='vertical', padding=[20, 40, 20, 20], spacing=15)
+        
+        # 타이틀 (PristonTale)
+        title_lbl = Label(text="PristonTale", font_size='32sp', size_hint_y=0.1)
+        if os.path.exists("font.ttf"): title_lbl.font_name = "CustomFont"
+        self.layout.add_widget(title_lbl)
 
-<CharInfoScreen>:
-    name: "char_info"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "케릭정보창 (4/3/5/5)"
-            left_action_items: [["arrow-left", lambda x: root.go_back()]]
-        ScrollView:
-            MDBoxLayout:
-                orientation: "vertical"
-                padding: "15dp"
-                spacing: "2dp"
-                id: info_container
+        # 검색창 영역 (이미지 9)
+        search_area = BoxLayout(size_hint_y=0.08, spacing=5)
+        self.search_in = CustomTextInput(hint_text="계정 검색...")
+        search_area.add_widget(self.search_in)
+        self.layout.add_widget(search_area)
 
-<EquipmentScreen>:
-    name: "equipment"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "케릭장비창 (11종 목록)"
-            left_action_items: [["arrow-left", lambda x: root.go_back()]]
-        ScrollView:
-            MDBoxLayout:
-                orientation: "vertical"
-                padding: "10dp"
-                spacing: "5dp"
-                id: equip_list
+        # 새 계정 만들기 버튼 (이미지 10)
+        add_btn = Button(text="+ 새 계정 만들기", size_hint_y=0.08, background_color=get_color_from_hex('#27ae60'))
+        if os.path.exists("font.ttf"): add_btn.font_name = "CustomFont"
+        self.layout.add_widget(add_btn)
 
-<InventoryScreen>:
-    name: "inventory"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "인벤토리창 (저장/삭제/수정)"
-            left_action_items: [["arrow-left", lambda x: root.go_back()]]
-        ScrollView:
-            MDList:
-                id: inv_list
+        # 계정 리스트 영역 (이미지 11)
+        self.scroll = ScrollView()
+        self.acc_list_layout = GridLayout(cols=1, size_hint_y=None, spacing=12)
+        self.acc_list_layout.bind(minimum_height=self.acc_list_layout.setter('height'))
+        self.scroll.add_widget(self.acc_list_layout)
+        self.layout.add_widget(self.scroll)
 
-<PhotoSelectScreen>:
-    name: "photo_select"
-    MDBoxLayout:
-        orientation: "vertical"
-        MDTopAppBar:
-            title: "사진선택창"
-            left_action_items: [["arrow-left", lambda x: root.go_back()]]
-        MDBoxLayout:
-            orientation: "vertical"
-            padding: "20dp"
-            spacing: "20dp"
-            MDLabel:
-                text: "핸드폰 사진 다중 선택 및 업로드/다운로드"
-                halign: "center"
-            MDRaisedButton:
-                text: "저장버튼"
-                pos_hint: {"center_x": .5}
-            MDRaisedButton:
-                text: "삭제버튼"
-                md_bg_color: 1, 0, 0, 1
-                pos_hint: {"center_x": .5}
-'''
+        self.add_widget(self.layout)
 
-class ErrorDialogContent(MDScreen):
-    error_text = StringProperty("")
-
-class MainScreen(MDScreen):
-    def on_enter(self):
-        self.ids.account_list.clear_widgets()
-        # [제1원칙 고착] 계정 ID 선택 목록 보존
-        for i in range(3):
-            self.ids.account_list.add_widget(OneLineListItem(text=f"계정 ID {i}", on_release=lambda x: self.go_char()))
-    def go_char(self): self.manager.current = "char_select"
-
-class CharSelectScreen(MDScreen):
-    def on_enter(self):
-        self.ids.char_slots.clear_widgets()
-        # [제1원칙 고착] 6개 슬롯 구조 보존
-        for i in range(1, 7):
-            self.ids.char_slots.add_widget(MDRaisedButton(text=f"슬롯 {i}", on_release=lambda x: self.go_info()))
-    def go_info(self): self.manager.current = "char_info"
-    def go_back(self): self.manager.current = "main"
-
-class CharInfoScreen(MDScreen):
-    # [제1원칙 고착] 4/3/5/5 배치 절대 수호
+# [제1원칙 고착] 캐릭터 정보창 (4/3/5/5 구조 절대 보존)
+class CharInfoScreen(BaseScreen):
     groups = [
         [('이름', ''), ('직위', ''), ('클랜', ''), ('레벨', '')], # 4
         [('생명력', ''), ('기력', ''), ('근력', '')],              # 3
         [('힘', ''), ('정신력', ''), ('재능', ''), ('민첩', ''), ('건강', '')], # 5
         [('명중', ''), ('공격', ''), ('방어', ''), ('흡수', ''), ('속도', '')]  # 5
     ]
-    def on_enter(self):
-        self.ids.info_container.clear_widgets()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.main_layout = BoxLayout(orientation='vertical', padding=15)
+        self.scroll = ScrollView()
+        self.info_container = BoxLayout(orientation='vertical', size_hint_y=None, spacing=2)
+        self.info_container.bind(minimum_height=self.info_container.setter('height'))
+        
         for i, group in enumerate(self.groups):
-            for label, val in group:
-                self.ids.info_container.add_widget(MDTextField(hint_text=label, text=val))
+            for label_text, val in group:
+                self.info_container.add_widget(CustomTextInput(hint_text=label_text, text=val))
             if i < len(self.groups) - 1:
-                self.ids.info_container.add_widget(Widget(size_hint_y=None, height="30dp"))
-        self.ids.info_container.add_widget(MDRaisedButton(text="장비창 이동", on_release=lambda x: self.go_equip()))
-    def go_equip(self): self.manager.current = "equipment"
-    def go_back(self): self.manager.current = "char_select"
+                self.info_container.add_widget(Label(size_hint_y=None, height="30dp"))
+        
+        self.scroll.add_widget(self.info_container)
+        self.main_layout.add_widget(self.scroll)
+        self.add_widget(self.main_layout)
 
-class EquipmentScreen(MDScreen):
-    # [제1원칙 고착] 11종 목록 절대 보존
-    items = ["한손무기", "두손무기", "갑옷", "방패", "장갑", "부츠", "암릿", "링1", "링2", "아뮬랫", "기타"]
-    def on_enter(self):
-        self.ids.equip_list.clear_widgets()
-        for item in self.items:
-            self.ids.equip_list.add_widget(MDTextField(hint_text=item))
-        self.ids.equip_list.add_widget(MDRaisedButton(text="인벤토리 이동", on_release=lambda x: self.go_inv()))
-    def go_inv(self): self.manager.current = "inventory"
-    def go_back(self): self.manager.current = "char_info"
-
-class InventoryScreen(MDScreen):
-    def on_enter(self):
-        self.ids.inv_list.clear_widgets()
-        for i in range(5):
-            # [제1원칙 고착] 저장/삭제 및 수정 로직 수호
-            item = TwoLineAvatarIconListItem(text=f"아이템 {i}", secondary_text="클릭 시 수정 및 사진 선택")
-            item.add_widget(IconLeftWidget(icon="content-save"))
-            item.on_release = self.go_photo
-            self.ids.inv_list.add_widget(item)
-    def go_photo(self): self.manager.current = "photo_select"
-    def go_back(self): self.manager.current = "equipment"
-
-class PhotoSelectScreen(MDScreen):
-    def go_back(self): self.manager.current = "inventory"
-
-class PT1App(MDApp):
+class PT1App(App):
     def build(self):
-        self.theme_cls.primary_palette = "BlueGray"
-        Builder.load_string(KV)
-        return MDScreenManager()
-    def show_error_popup(self, msg):
-        content = ErrorDialogContent(error_text=msg)
-        self.dialog = MDDialog(title="시스템 오류", type="custom", content_cls=content)
-        self.dialog.open()
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(CharInfoScreen(name='char_info'))
+        return sm
 
 if __name__ == "__main__":
     PT1App().run()
